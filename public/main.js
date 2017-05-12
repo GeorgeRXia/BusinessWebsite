00549245078f7daeba4b215633eb415b9e40e001
Ethan Megill <megill1352@gmail.comm>
2017-05-12 14:53:52 -0400

    routing issue

--- public/main.js
+++ public/main.js
@@ -1,20 +1,20 @@
 var carousel=document.getElementsByClassName("pic1")[0]
 
  function carouselStart() {
-    carousel.style.backgroundImage="url('pic1.jpg')"
+    carousel.style.backgroundImage="url('../images/pic1.jpg')"
     window.setTimeout(pic2,3000)
 }
 
  carouselStart();
 
  function pic2(){
-    carousel.style.backgroundImage="url('pic2.jpg')"
+    carousel.style.backgroundImage="url('../images/pic2.jpg')"
     window.setTimeout(pic3,3000)
 
  }
 
   function pic3(){
-    carousel.style.backgroundImage="url('pic3.jpg')"
+    carousel.style.backgroundImage="url('../images/pic3.jpg')"
     window.setTimeout(carouselStart,3000)
 
  }
