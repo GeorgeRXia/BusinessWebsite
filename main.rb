a833f46a152981a44e1cb77e77eb1dec69b828d6
tomatoandcheerio <Georxia@gmail.com>
2017-05-12 11:29:16 -0400

    Intial Commit

---
+++ main.rb
@@ -0,0 +1,23 @@
+require 'sinatra'
+
+get '/home' do
+
+	erb :home
+end
+
+
+get '/contact' do
+
+	erb :contact
+end
+
+get '/product' do
+	erb :contact
+
+
+end
+
+
+get '/image' do
+	erb :image
+end
